f84b17096c28c1945d5d1eb783ce5ae1c198a62b
Godswill <godswillijoma@gmail.com>
2022-09-25 13:08:15 +0100

    food-delivery

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
